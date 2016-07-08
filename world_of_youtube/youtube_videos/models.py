aef7dfe31142fc67f0893b6e487bc8cb91a7e10e
DjangoGirls <you@example.com>
2016-07-08 01:10:01 -0400

    added model video and synchronized database accordingly

--- world_of_youtube/youtube_videos/models.py
+++ world_of_youtube/youtube_videos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Video(models.Model):
+    video_title = models.CharField(max_length=250)
+    video_channel = models.CharField(max_length=250)
+    video_duration = models.DurationField()
+    video_upload_date = models.DateField()
+    video_thumbnail = models.ImageField()
