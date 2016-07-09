97db165ac460d3cdc104ebee2837bc66052b3b89
DjangoGirls <you@example.com>
2016-07-08 22:36:17 -0400

    Added constants.py to hold variables for the choice field, and modified other files accordingly

--- world_of_youtube/youtube_videos/forms.py
+++ world_of_youtube/youtube_videos/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 
+from .constants import COUNTRIES
+
 class QueryForm(forms.Form):
-    country = forms.ChoiceField()
+    country = forms.ChoiceField(choices=COUNTRIES, required=True)
     category = forms.ChoiceField()
     number_of_videos_to_return = forms.IntegerField(max_value=50)
--- world_of_youtube/youtube_videos/views.py
+++ world_of_youtube/youtube_videos/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from .forms import QueryForm
 
 def index(request):
-    pass
+    if request.method == 'POST':
+        form = QueryForm(request.POST)
+        
+        if form.is_valid():
+            country = form.cleaned_data['country']
+            category = form.cleaned_data['category']
+            n = form.cleaned_data['number_of_videos_to_return']
+            str = "%s %s %s " % (country, category, n)
+            return HttpResponse(str)
+            
+    else:
+        form = QueryForm()
+    
+    return render(request, 'youtube_videos/index.html', {'form': form})
+            
     
