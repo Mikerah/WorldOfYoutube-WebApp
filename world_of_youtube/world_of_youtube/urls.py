f7f0e627ba1e0d7974a87cfd855ea7be058e8abe
DjangoGirls <you@example.com>
2016-07-10 14:34:32 -0400

    changed url configuration

--- world_of_youtube/world_of_youtube/urls.py
+++ world_of_youtube/world_of_youtube/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^youtube_videos/', include('youtube_videos.urls')),
+    url(r'^$', include('youtube_videos.urls')),
     url(r'^admin/', admin.site.urls),
     
 ]
